6f7a762fe4c06776f8226198a36ceb9bddf76a08
Siketyan <siketyan@sikeserver.com>
2018-07-03 17:54:49 +0900

    Add login logic

--- src/js/local-connect.js
+++ src/js/local-connect.js
@@ -13,6 +13,23 @@ $(".keyboard > .button").click((e) => {
 });
 
 $("#backspace").click(() => {
-    const textarea = $("#token");
-    textarea.val(textarea.val().substring(0, textarea.val().length - 1));
+    const token = $("#token");
+    token.val(token.val().substring(0, token.val().length - 1));
+});
+
+$("#login").click(() => {
+    const token = $("token");
+
+    fetch("https://api.local-connect.ga/sessions/create", {
+        body: "token=" + token.val(),
+        method: "POST",
+        mode: "cors"
+    })
+        .then(response => response.json())
+        .then(json => {
+            console.log(json);
+        })
+        .catch(error => {
+            console.error(error);
+        });
 });
