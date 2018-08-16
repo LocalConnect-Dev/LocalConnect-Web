d7ab7f11aa42eea103531efbb8ca3147a0670377
Siketyan <siketyan@sikeserver.com>
2018-08-16 18:21:12 +0900

    Add event view

--- src/js/event.js
+++ src/js/event.js
@@ -1,21 +1,22 @@
 $(() => {
-    console.log("Loading template of events");
-    $("#wrapper").load("view/events.html", () => {
-        new APICall("events/list")
+    console.log("Loading template of event");
+    $("#wrapper").load("view/event.html", () => {
+        new APICall("events/show")
             .authorize()
-            .onSuccess(events => {
+            .params({
+                id: URI(location.href).search(true).id
+            })
+            .onSuccess(event => {
                 new Vue({
-                    el: "#events",
-                    data: {
-                        events: events
-                    },
+                    el: "#event",
+                    data: event,
                     filters: {
+                        moment: date => moment.unix(date).fromNow(),
                         year: date => moment.unix(date).format("Y"),
                         month: date => moment.unix(date).format("MM"),
                         day: date => moment.unix(date).format("DD"),
                         dayOfWeek: date => moment.unix(date).format("dd"),
-                        time: date => moment.unix(date).format("kk:mm"),
-                        summary: str => str.substr(0, 32) + "…"
+                        time: date => moment.unix(date).format("kk:mm")
                     }
                 });
 
