8a200db7668eca6d29990cbfcc1657bd9b930af4
Siketyan <siketyan@sikeserver.com>
2018-08-16 17:18:51 +0900

    Add create-board view

---
+++ src/js/create-board.js
@@ -0,0 +1,43 @@
+$(() => {
+    console.log("Loading template of create-board");
+    $("#wrapper").load("view/create-board.html", () => {
+        hideLoader();
+    });
+});
+
+onClick("#submit", () => {
+    showLoader();
+
+    const title = $("#title").val();
+    const content = $("#content").val();
+
+    new APICall("documents/create")
+        .authorize()
+        .post()
+        .params({
+            title: title,
+            content: content
+        })
+        .onSuccess(document => {
+            new APICall("boards/create")
+                .authorize()
+                .post()
+                .params({
+                    document: document.id
+                })
+                .onSuccess(board => {
+                    $("#created-board")
+                        .modal({
+                            closable: false,
+                            onApprove: () => {
+                                showLoader();
+                                move(URI("/board.view?id=" + board.id, location.href));
+                            }
+                        })
+                        .modal("show");
+
+                    hideLoader();
+                })
+        })
+        .execute();
+});
