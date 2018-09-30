06adc5b4d191b0c1357a53cae65bfb8fe3355956
Siketyan <siketyan@sikeserver.com>
2018-09-30 18:03:52 +0900

    Add visual editor

--- src/js/create-board.js
+++ src/js/create-board.js
@@ -1,6 +1,28 @@
 $(() => {
     console.log("Loading template of create-board");
     $("#wrapper").load("view/create-board.html", () => {
+        if (window.ckeditor) {
+            window.ckeditor.destroy()
+                .then(() => {
+                    window.ckeditor = null;
+                })
+                .catch(error => {
+                    console.error(error);
+                });
+        }
+
+        ClassicEditor
+            .create(document.querySelector("#editor"), {
+                language: "ja"
+            })
+            .then(editor => {
+                console.log(editor);
+                window.ckeditor = editor;
+            })
+            .catch(error => {
+                console.error(error);
+            });
+
         hideLoader();
     });
 });
@@ -9,7 +31,7 @@ onClick("#submit", () => {
     showLoader();
 
     const title = $("#title").val();
-    const content = $("#content").val();
+    const content = window.ckeditor.getData();
 
     new APICall("documents/create")
         .authorize()
