143535dba79db76a7e2848211830ee671cde6d6b
Siketyan <siketyan@sikeserver.com>
2018-10-14 03:09:52 +0900

    Add avatar uploading

--- src/js/create-profile.js
+++ src/js/create-profile.js
@@ -6,7 +6,10 @@ $(() => {
             .onSuccess(profile => {
                 new Vue({
                     el: "#create-profile",
-                    data: profile
+                    data: {
+                        avatar: window.user.avatar,
+                        profile: profile
+                    }
                 });
 
                 hideLoader();
@@ -48,4 +51,50 @@ onClick("#submit", () => {
             hideLoader();
         })
         .execute();
+});
+
+onClick("#open", () => {
+    $("<input>")
+        .attr("type", "file")
+        .attr("accept", ".jpg, image/jpeg")
+        .on("change", event => {
+            showLoader();
+
+            const reader = new FileReader();
+            reader.onload = () => {
+                new APICall("images/create")
+                    .authorize()
+                    .post()
+                    .body(reader.result)
+                    .onSuccess(image => {
+                        $("#preview").attr("src", "//api.local-connect.ga/images/show?id=" + image.id);
+
+                        new APICall("users/set_avatar")
+                            .authorize()
+                            .post()
+                            .params({
+                                avatar: image.id
+                            })
+                            .onSuccess(user => {
+                                $("#set-avatar").clone().prop("id", "set-avatar-instance").appendTo("body");
+
+                                const selector = "#set-avatar-instance";
+                                const element = $(selector);
+                                element.modal({
+                                    closable: false,
+                                    onHidden: () => {
+                                        finalizeModal();
+                                    }
+                                }).modal("show");
+
+                                hideLoader();
+                            })
+                            .execute();
+                    })
+                    .execute();
+            };
+
+            reader.readAsArrayBuffer(event.target.files[0]);
+        })
+        .click();
 });
